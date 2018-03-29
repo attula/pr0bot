b0171ca89b7b8e780763f30ac4b17bbba0527b4f
attula <arthur.debel@tu-dortmund.de>
2018-03-29 17:25:04 +0200

    add .gitignore

--- bot.py
+++ bot.py
@@ -1,1 +1,34 @@
-import discord.py
+import discord
+import asyncio
+
+with open('token', 'r') as file:
+    token=file.read().replace('\n', '')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    print(token)
+    
+@client.event
+async def on_message(message):
+    if message.content.startswith('http://pr0gramm.com/'):
+       	message.
+
+
+
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+        await client.edit_message(tmp, 'You have {} messages.'.format(counter))
+    elif message.content.startswith('!sleep'):
+        await asyncio.sleep(5)
+        await client.send_message(message.channel, 'Done sleeping')
+
+client.run(token)
